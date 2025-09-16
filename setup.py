080d8f14a7c83adc8fd2069e1c1fe6b7e0132a8c
Ahmad Zeidan <ahmad.zeidan@aem-et.de>
2025-09-16 15:56:44 +0200

    Fixed path issues for different directories

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mkdocs-auto-figure-list",
-    version="0.2.1",
+    version="0.2.2",
     description="Auto creation for figures in MkDocs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
